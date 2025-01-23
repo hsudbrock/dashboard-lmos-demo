90e3820083bec6ff115cc28f3985eca21fe44eec
Henning Sudbrock <henning.sudbrock@telekom.de>
2025-01-23 23:17:38 +0100

    helm chart naming

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -o logo.png "https://avatars.githubusercontent.com/u/165814394?s=200&v=4"
-helm upgrade --install homer .
+helm upgrade --install dashboard-lmos-demo .
 sleep 5
-kubectl port-forward svc/homer 12345:8080
+kubectl port-forward svc/dashboard-lmos-demo 12345:8080
